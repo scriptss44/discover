28c37b09d462bf2c756fbc0fc925234476b62391
scriptss44 <makepyscript@gmail.com>
2025-09-19 21:01:09 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.css': {
+          loaders: ['css-loader'],
+          as: '*.css',
+        },
+      },
+    },
+  },
+};
 
 module.exports = nextConfig;
