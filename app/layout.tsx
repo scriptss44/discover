03ac34ae7ad38881aeb06a6e68acd3c1260ce5a6
scriptss44 <makepyscript@gmail.com>
2025-09-19 23:59:38 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>{children}</body>
