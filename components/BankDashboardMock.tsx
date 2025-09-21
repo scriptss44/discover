3a17cb8bd11342229edb36b1f99523021654b329
scriptss44 <makepyscript@gmail.com>
2025-09-21 03:39:43 +0200

    Update BankDashboardMock.tsx

--- components/BankDashboardMock.tsx
+++ components/BankDashboardMock.tsx
@@ -320,7 +320,7 @@ export default function BankDashboardMock() {
                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"/>
                 <circle cx="12" cy="10" r="3"/>
               </svg>
-              <span className="text-gray-900 font-medium">DISCOVER FINANCIAL SERVICES</span>
+              <span className="text-gray-900 font-medium">10425 WOODBINE ST APT 17 LOS ANGELES CA 90034-3735 USA</span>
             </div>
           </div>
 
